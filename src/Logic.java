5be67a8ef8b98cb205f722a2e6ba99d457b1bb7d
etssu <brominaroman@gmail.com>
2025-11-11 13:50:59 +0100

    add symbol calculation functions

--- src/Logic.java
+++ src/Logic.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Logic {
     private int WPM;
     private float accuracy;
@@ -8,16 +6,36 @@ public class Logic {
     public int getWPM() {
         return WPM;
     }
-
-    public int findWPM(int time, int symbols){
-        return symbols / 5 / time;
+    // Main functions
+    public int findWPM(int time, int symbolCount){
+        return symbolCount / 5 / time;
     }
 
-    public float calculateAccuracy(float rightSymbols, float wrongSymbols){
-       return rightSymbols / wrongSymbols * 100;
+    public float calculateAccuracy(float correctSymbolsCount, float wrongSymbolsCount){
+       return correctSymbolsCount / wrongSymbolsCount * 100;
     }
+
     public float calculateAdjustedWPM(){
         return accuracy * WPM;
+    }
+
+    // Symbols Functions
+    public int getSymbolsCount(String text){
+        return text.length();
+    }
+
+    public int getCorrectSymbolsCount(int symbolCount, String userInput, String text){
+        int correctSymbolsCount = 0;
+        for (int i = 0; i < symbolCount; i++){
+            if (userInput.charAt(i) != text.charAt(i)){
+                correctSymbolsCount++;
+            }
+        }
+        return correctSymbolsCount;
+    }
+
+    public int getWrongSymbolsCount(int symbolCount, int correctSymbolCount){
+        return symbolCount - correctSymbolCount;
     }
 
 }
