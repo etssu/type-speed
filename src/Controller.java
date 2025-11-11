3eb63a56ff52ba209d6b2a7888b0fce10ce2b361
etssu <brominaroman@gmail.com>
2025-11-11 14:34:49 +0100

    add timer modes

--- src/Controller.java
+++ src/Controller.java
@@ -8,5 +8,27 @@ public class Controller {
         int seconds = sc.nextInt();
         TimerThread timer = new TimerThread(seconds);
         timer.run();
+
+        timer.startTimer();
+        // add main logic here
+            // while !isTimeUp:
+                // print original text
+                // get user input
+
+        timer.stopTimer();
+        // print elapsed time
+        // print wpm, accuracy, adjusted wpm
+
     }
 }
+// Notes
+/* ask for time (30 or 60 seconds)
+* print mode and time + buttons "start" and "cancel"
+* start the test, then check "isTimeUp"
+* if !isTimeUp -> continue getting user input
+* if isTimeUp -> stop the timer, print elapsed time, wpm, accuracy and adjusted wpm
+* print the menu:
+*   - play again
+*   - save results
+*   - quit
+* */
--- src/TimerThread.java
+++ src/TimerThread.java
@@ -3,6 +3,9 @@ import java.time.Duration;
 
 public class TimerThread implements Runnable {
     private final long seconds;
+    private boolean timeUp = false;
+    private long startTime;
+    private long endTime;
 
     public TimerThread(int seconds) {  // set seconds (can be 30 or 60)
         this.seconds = seconds;
@@ -11,8 +14,25 @@ public class TimerThread implements Runnable {
     public void run() {
         try{
             Thread.sleep(seconds * 1000); // sleep for (seconds * 1000) seconds
+            timeUp = true;
             System.out.println(seconds + " seconds have passed!");
         }
         catch (InterruptedException e){}
+    }
+
+    public boolean isTimeUp(){
+        return timeUp;
+    }
+
+    public void startTimer(){
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stopTimer(){
+        endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime(){
+        return endTime - startTime;
     }
 }
