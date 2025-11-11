1fc1cea5dca8bcfcf5b428a34ba6b1977a2545d3
etssu <brominaroman@gmail.com>
2025-11-11 13:13:45 +0100

    add calculation functions

--- src/Logic.java
+++ src/Logic.java
@@ -9,4 +9,15 @@ public class Logic {
         return WPM;
     }
 
+    public int findWPM(int time, int symbols){
+        return symbols / 5 / time;
+    }
+
+    public float calculateAccuracy(float rightSymbols, float wrongSymbols){
+       return rightSymbols / wrongSymbols * 100;
+    }
+    public float calculateAdjustedWPM(){
+        return accuracy * WPM;
+    }
+
 }
